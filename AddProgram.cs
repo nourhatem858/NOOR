956dd1b9004043c053daddfa7fc21710c79ed88b
nour <nourhatem.522082@gmail.com>
2023-12-03 11:07:15 +0200

    frist

---
+++ AddProgram.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Globalization;
+
+namespace first
+{
+    public class AddProgram
+    {
+        public bool IsGreaterThanZero(int value)
+        {
+            if (value <= 0)
+                //Console.WriteLine("Number is greater than zero");
+                return true;
+            else
+                Console.WriteLine("Number is less than zero");
+                return false;
+        }
+
+        public int Addition(int x, int y)
+        {
+            int res = x + y;
+            return res;
+        }
+
+
+        public string NameOfMonth(int MonthIndex)
+        {
+            string MonthName = "";
+            if (MonthIndex > 0 && MonthIndex < 12)
+                MonthName = CultureInfo.CurrentCulture.DateTimeFormat.GetMonthName(MonthIndex);
+            else
+                MonthName = "Not a Valid Month";
+            return MonthName;
+        }
+    } 
+}
+
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace first
+{
+    public class Program
+    {
+        // Main Method with swapping of modifiers
+       public static void Main()
+        {
+            Console.WriteLine("Heyy!");
+        }
+    }
+}
